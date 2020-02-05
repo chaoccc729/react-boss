203d5f22384fbeabeaeeb7dc649615b4847449f7
chaoccc729 <1547747476@qq.com>
2020-02-05 18:01:27 +0800

    action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,11 +3,40 @@
 import {
   reqRegister,
   reqLogin,
-} from '../api'
+} from '../api';
+
+import{
+  AUTH_SUCCESS,
+  ERROR_MSG
+} from './action-types'
+
+//授权成功的同步action
+const authSuccess = (user) => ({type: AUTH_SUCCESS, data: user});
+//错误提示信息同步action
+const errorMsg = (msg) => ({type: ERROR_MSG, data: msg});
 //注册异步
 export const reqRegister = () => {
-  return dispatch => {
+  return async dispatch => {
     //发送注册请求
-
+    const response = await reqRegister(user);
+    const result = response.data;
+    if(response.code === 0){
+      dispatch(authSuccess(result.data));//分发授权成功同步action
+    } else {
+      dispatch(errorMsg(result.msg)); //分发失败提示信息同步action
+    }
   }
 } 
+
+//登录异步
+export const reqLogin = () => {
+  return async dispatch => {
+    //发送注册请求
+    const response = await reqLogin(user);
+    if (response.code == 0) {
+
+    } else {
+
+    }
+  }
+}
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,6 +1,9 @@
 // 包含多个reducer函数（根据旧state和action返回新state）
 import {combineReducers} from 'redux';
-
+import {
+  AUTH_SUCCESS,
+  ERROR_MSG,
+} from './action-types';
 const initUser = {
   username:'',
   type:'',
@@ -8,6 +11,10 @@ const initUser = {
 }
 function user(state=initUser, action) {//产生user状态
   switch (action.type) {
+    case AUTH_SUCCESS:
+      return {...state, ...action.data};
+    case ERROR_MSG:
+      return {...state, msg: action.data};
     default:
       return state;
   }
